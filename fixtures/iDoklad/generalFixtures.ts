44c0d100b64e14e4d0d4ce0962a3799d6800a275
martinpur <m_pur@utb.cz>
2025-01-20 20:58:20 +0100

    optimised create contact test

--- fixtures/iDoklad/generalFixtures.ts
+++ fixtures/iDoklad/generalFixtures.ts
@@ -3,8 +3,34 @@ import { test as baseTest } from '@playwright/test'
 export const test = baseTest.extend({
   getByDataUiId: async ({ page }, use) => {
     const getByDataUiId = (data_ui_id) => page.locator(`[data-ui-id="${data_ui_id}"]`)
-        await use(getByDataUiId)
-    },
+    await use(getByDataUiId)
+  },
+
+  login: async ({ page, getByDataUiId }, use) => {
+    const login = async (email, password) => {
+      await page.goto('https://app.idoklad.cz/Account/Login', { waitUntil: 'load' })
+
+      const emailField = getByDataUiId('csw-username')
+      const passwordField = getByDataUiId('csw-password')
+      const loginButton = getByDataUiId('csw-login-button')
+
+      await emailField.waitFor({ state: 'visible' })
+      await passwordField.waitFor({ state: 'visible' })
+      await loginButton.waitFor({ state: 'visible' })
+
+      await emailField.fill(email)
+      await passwordField.fill(password)
+
+      await page.route('**/api/Billing/GetDashboardSubscription', (route) => route.continue())
+      const [response] = await Promise.all([
+        page.waitForResponse('**/api/Billing/GetDashboardSubscription'),
+        loginButton.click(),
+      ])
+      return response
+    }
+
+    await use(login)
+  },
 })
 
 export const expect = test.expect
--- tests/iDoklad.spec.ts
+++ tests/iDoklad.spec.ts
@@ -1,9 +1,11 @@
-import { test, expect } from '../fixtures/iDoklad/authFixtures'
+import { test, expect } from '../fixtures/iDoklad/generalFixtures'
 import loginData from '../fixtures/iDoklad/login.json'
+import contacts from '../fixtures/iDoklad/contacts.json'
+import domData from '../fixtures/iDoklad/domData.json'
 
 // npx playwright codegen https://app.idoklad.cz/Account/Login - debug selectors
 
-test('Login with valid credentials and block Clarity requests', async ({ login, page }) => {
+test.beforeEach('Login with valid credentials and block Clarity requests', async ({ login, page }) => {
   await page.route('https://www.clarity.ms/**', (route) => {
     route.fulfill({
       status: 403,
@@ -17,4 +19,67 @@ test('Login with valid credentials and block Clarity requests', async ({ login, 
 
   // Close tip dialog
   await page.locator('.dialog-buttons > div > button').click()
+})
+
+test('Create contact', async ({ page, getByDataUiId }) => {
+
+  // Open new contact form
+  await getByDataUiId('csw-new-item').click()
+  const createContact = await getByDataUiId('csw-new-item-contact')
+  await expect(createContact).toBeVisible()
+  await createContact.click()
+
+  const sectionTitle = page.locator('.heading-wrapper h1')
+  await expect(sectionTitle).toBeVisible()
+  await expect(sectionTitle).toHaveText('Nový kontakt')
+
+  const saveContact = getByDataUiId('csw-save-new-contact')
+  await expect(saveContact).toBeVisible()
+  await saveContact.click()
+
+  const validations = page.locator('.errors-wrapper')
+  await expect(validations).toBeVisible()
+
+  const countryButton = page.locator('[name="CountryId"]').locator('..').locator('button')
+  await expect(countryButton).toBeVisible()
+  await countryButton.click()
+
+  const countrySearch = page.locator('.k-list-filter')
+  await expect(countrySearch).toBeVisible()
+  await countrySearch.type(contacts[0].countrySearchValue)
+
+  const targetCountry = page.locator('ul[role="listbox"] li')
+  await expect(targetCountry).toHaveCount(1)
+  await expect(targetCountry).toContainText(contacts[0].country)
+  await targetCountry.click()
+
+  const companyName = page.locator('[name="CompanyName"]')
+  await expect(companyName).toBeVisible()
+  await companyName.fill(contacts[0].name)
+
+  const identificationNumber = page.locator('[name="IdentificationNumber"]')
+  await expect(identificationNumber).toBeVisible()
+  await identificationNumber.fill(contacts[0].identificationNumber)
+
+  // Check if there is same existing contact
+  const checkDupliciteIcoTimePromise = page.waitForResponse(
+    '**/api/Contact/CheckEmailDuplicity?contactEmail=&contactIn=' + contacts[0].identificationNumber + '&contactId=0'
+  )
+  const saveContactTimePromise = page.waitForResponse('**/api/Contact/Create')
+
+  await saveContact.click()
+  await checkDupliciteIcoTimePromise
+
+  const btnText = await saveContact.textContent()
+  if (btnText.trim() === domData.saveDuplicateContact) {
+    // Duplicate contact found
+    await saveContact.click()
+  }
+
+  await saveContactTimePromise
+
+  await expect(validations).not.toBeVisible()
+  const toastMessage = getByDataUiId('csw-toast-message')
+  await expect(toastMessage).toBeVisible()
+  await expect(toastMessage).toContainText(contacts[0].name)
 })
